240b52713b6342b1d9e64c16effee7ad9b88c527
Rabin Mainali <rabinmainali1999@gmail.com>
2024-08-20 14:17:15 +0545

    added user authentication

--- Django/Django/settings.py
+++ Django/Django/settings.py
@@ -131,3 +131,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+LOGIN_URL = '/accounts/login'
+LOGIN_REDIRECT_URL = '/tweet/'
+LOGOUT_REDIRECT_URL = '/tweet/'
